22ece91558a7959ff516e21bce547d8c1e2ce412
ceejayi <cjloveiya@gmail.com>
2025-10-25 18:33:07 +0800

    Welcome.vb (print hello mico)

--- Welcome.vb
+++ Welcome.vb
@@ -2,6 +2,7 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         AdminLogin.Show()
         Me.Hide()
+        Print("hello mico")
     End Sub
 
     Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
