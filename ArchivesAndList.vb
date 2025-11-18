807026edbede607bf864b5275f13851a81fac026
ceejayi <cjloveiya@gmail.com>
2025-11-19 04:19:46 +0800

    Revert "deleted old forms"
    This reverts commit b3c3ad922707997678308855bb0312e2dfd481e7.

---
+++ ArchivesAndList.vb
@@ -0,0 +1,10 @@
+﻿Public Class ArchivesAndList
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles bttnBack.Click
+        AdminWelcome.Show()
+        Me.Hide()
+    End Sub
+End Class
