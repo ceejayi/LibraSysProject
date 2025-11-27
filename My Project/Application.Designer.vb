c02119a48d8e6fafe032d7e17e14aea60bee76cd
nicandromico-maker <nicandro.mico@gmail.com>
2025-11-27 17:49:10 +0800

    FORMSSS

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -33,7 +33,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.LibraSys.BookManagement
+            Me.MainForm = Global.LibraSys.Welcome
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
